b1d8b773659e7d334836fa0c232a6ce2ef1ad8de
Shakila <shakila.mehri.98@gmail.com>
2024-06-17 18:11:02 +0330

    header updated

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,22 +2,185 @@
 
 import React, { useState } from "react";
 import Link from "next/link";
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaChevronRight } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
 import styles from "../styles/header.module.css";
 
 
 const Header: React.FC = () => {
+  const [isCategoryDropdownOpen, setIsCategoryDropdownOpen] = useState(false);
+  const [activeCategory, setActiveCategory] = useState<number | null>(null);
+  const [activeSubcategory, setActiveSubcategory] = useState<number | null>(null);
   const [isCityDropdownOpen, setIsCityDropdownOpen] = useState(false);
+
+  const toggleCategoryDropdown = () => {
+    setIsCategoryDropdownOpen(!isCategoryDropdownOpen);
+  };
+
+  const handleCategoryHover = (index : number) => {
+    setActiveCategory(index);
+    setActiveSubcategory(null);
+  };
+
+  const handleSubcategoryHover = (index : number) =>{
+    setActiveSubcategory(index);
+  }
 
   const toggleCityDropdown = () => {
     setIsCityDropdownOpen(!isCityDropdownOpen);
   };
 
+  const categories = [
+  { name: "موبایل", subcategories: [
+    {
+      name: "برندهای مختلف گوشی موبایل",
+       options:["گوشی آیفون", "گوشی سامسونگ", "گوشی شیائومی"],
+      },
+      {
+        name: "گوشی براساس قیمت",
+        options:["گوشی موبایل ارزان", "گوشی موبایل قسطی", "گوشی تا 2 میلیون تومن"],
+      },
+    {
+      name: "گوشی براساس عملکرد",
+      options:["گوشی گیمینگ", "گوشی دکمه‌ای ", "گوشی ضدآب"],
+    }]},
+  { name: "کتاب، لوازم تحریر و هنر", subcategories: [
+    {
+       name:"کتاب و مجله", 
+    options:["کتاب چاپی", "مجلات داخلی و خارجی"],
+    },
+    {
+      name:"کتاب صوتی",
+      options:[""],
+    },
+    {
+      name: "محتوای آموزشی",
+        options:["آموزش موسیقی", "آموزش ورزش و سرگرمی","آموزش زبان"],
+    },
+    {
+      name:"لوازم تحریر",
+        options:["لوازم اداری و اقلام مصرفی", "کیف، کوله‌پشتی و جامدادی", "چراغ مطالعه"],
+    }]},
+  { name: "کالای دیجیتال",
+   subcategories: [
+    {
+     name: "لپ‌تاپ",
+     options:["لپ‌تاپ ایسوس", "لپ‌تاپ لنوو", "لپ‌تاپ دل"],
+    }, 
+   {
+   name: "لوازم جانبی لپ‌تاپ",
+    options:["شارژر لپ‌تاپ", "کیف لپ‌تاپ", "کول پد"],
+  },
+  {
+   name: "هدفون",
+    options:["هدفون بی‌سیم", "هدفون اپل(ایرپاد)", "هدفون سونی"],
+  },
+  {
+    name:  "پرینتر",
+    options:["پرینتر سه بعدی", "پرینتر حرارتی", "پرینتر رنگی"],
+  }]},
+  { name: "خانه و آشپزخانه", subcategories: [
+    { name: " ظروف پخت و پز",
+    options: ["قابلمه و تابه", "زودپز", "سرویس پخت و پز"],
+  },
+  {
+    name: "ابزار آشپزی",
+    options: ["ست کفگیر و ملاقه", "چاقو" ,"تخته گوشت"],
+  },
+  {
+    name: "مبلمان و دکوراسیون خانگی",
+    options: ["پاف و بین‌بگ", "مبل", "کتابخانه"],
+  },
+  {
+    name: "حمام",
+    options: ["حوله", "دمپایی", "لیف"],
+  }]},
+  { name: "لوازم خانگی برقی", subcategories: [
+    { 
+      name: "صوتی تصویری",
+      options: ["", "", ""],
+    },
+    {
+      name: "یخچال فریزر",
+      options: ["یخچال فریزر ساید بای ساید", "یخچال فریزر دوقلو", "یخچال فریزر هتلی"],
+    },
+    {
+      name: "جارو برقی",
+      options: ["جارو شارژی", "لوازم جانبی جارو‌برقی و بخارشور"],
+    },
+    {
+      name: "ماشین لباسشویی",
+      options: ["ماشین لباسشویی پاکشوما", "ماشین لباسشویی دوو", "ماشین لباسشویی اسنوا"],
+    }]},
+  { name: "مد و پوشاک", subcategories: [
+  { name: "لباس زنانه",
+  options: ["هودی زنانه", "سویشرت زنانه", "دامن زنانه"],
+  },
+  {
+    name: "لباس مردانه", 
+    options: ["تی‌شرت مردانه", "پیراهن مردانه", "شلوار مردانه"],
+  },
+  {
+    name:  "پالتو، بارانی و کاپشن مردانه",
+    options: ["پالتو مردانه", "کاپشن مردانه", "کت چرم مردانه"],
+  },
+  {
+    name: "لباس دخترانه",
+    options: ["بلوز دخترانه", "شومیز دخترانه"],
+  }]},
+  { name: "ساعت، طلا و جواهرات", subcategories: [
+  { name: "شمش و پلاک طلا",
+    options: ["", "", ""],
+  },
+  { name:  "سکه طلا",
+    options: ["", "", ""],
+  },
+  { name: "طلای آب شده",
+    options: ["", "", ""],
+  }]},
+  { name: "آرایشی بهداشتی", subcategories: [
+    {
+      name: "لوازم آرایشی", 
+        options:["آرایش صورت", "آرایش چشم", "آرایش ابرو"],
+    },
+    {
+      name: "مراقبت پوست",
+        options:["کرم ضدآفتاب", "کرم مرطوب‌کننده و نرم‌کننده", "ماسک صورت و بدن"],
+    },
+    {
+      name: "مراقبت و زیبایی مو",
+        options:["شامپو و مراقبت مو", "آرایش مو"],
+    }]},
+  { name: "تجهیزات پزشکی و سلامت", subcategories: [
+  { 
+    name: "تجهیزات پزشکی",
+    options: ["کنترل دیابت", "لوازم پزشک", "ماساژور"],
+  },
+  { 
+    name:  "مکمل غذایی",
+    options: ["", "", ""],
+  },
+  { 
+    name: "مکمل ورزشی",
+    options: ["پروتئین", "آمینواسید", "کراتین"],
+  }]},
+  { name: "ورزش و سفر", subcategories: [
+  {
+    name: "پوشاک ورزشی زنانه",
+    options: ["", "", ""],
+  },
+  {
+    name: "پوشاک ورزشی مردانه",
+    options: ["", "", ""],
+  },
+  {
+    name: "کفش ورزشی مردانه",
+    options: ["", "", ""],
+  }]},
+  ];
   const cities = ["آذربایجان شرقی", "آذربایجان غربی", "اردبیل", "اصفهان", "البرز", "ایلام", "بوشهر"];
   return (
     <header className={styles.header}>
-      <nav>
         <div>
           <div className={styles.headerImage}>
             <img src="/images/header.gif" alt="header " />
@@ -52,18 +215,56 @@ const Header: React.FC = () => {
               <FaCartShopping className={styles.cartIcon}/>
             </Link>
           </div>
-          {/* Nav lists */}
-          <div className={styles.navLists}>
+          {/* NavBar */}
+          <div className={styles.container}>
+            <nav className={styles.navLists}>
             <ul className={styles.navList}>
-              <img className={styles.menuImg}
+              <img className={styles.menuIist}
                 src="/images/menu.png"
                 alt="categories"
+                onClick={toggleCategoryDropdown}
               />
               <li className={styles.linkLists}>
-                <Link href="#" className={styles.navLinks}> 
                 دسته‌بندی کالاها
-                </Link>
-              </li>
+                {isCategoryDropdownOpen && (
+                  <ul className={styles.dropdownList}>
+                    {categories.map((category, index) => (
+                      <li
+                        key={index}
+                        className={styles.dropdownItem}
+                        onMouseEnter={() => handleCategoryHover(index)}
+                        onMouseLeave={() => handleCategoryHover(null)}
+                      >
+                        <FaChevronRight className={styles.dropdownIcon} />
+                        {category.name}
+                        {activeCategory === index && (
+                          <ul className={styles.subDropdownList}>
+                            {category.subcategories.map((subcategory, subIndex) => (
+                              <li
+                                key={subIndex}
+                                className={styles.subDropdownItem}
+                                onMouseEnter={() => handleSubcategoryHover(subIndex)}
+                                onMouseLeave={() => handleSubcategoryHover(null)}
+                              >
+                                {subcategory.name}
+                                {activeSubcategory === subIndex && (
+                                  <ul className={styles.subDropdownList}>
+                                    {subcategory.options.map((option, optionIndex) => (
+                                      <li key={optionIndex} className={styles.subDropdownItem}>
+                                        {option}
+                                      </li>
+                                    ))}
+                                  </ul>
+                                )}
+                              </li>
+                            ))}
+                          </ul>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>    
               <span className={styles.spanLink}>|</span>
               <li className={styles.linkLists}>
                 <Link href="#">شگفت‌انگیزها</Link>
@@ -111,9 +312,9 @@ const Header: React.FC = () => {
                 </div>
               </li>
             </ul>
+            </nav>
           </div>
         </div>
-      </nav>
     </header>
   );
 };
